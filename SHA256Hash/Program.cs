e69e30b253790a7c1a847b21dd5b909a7d85534f
logan1.max@gmail.com <logan1.max@gmail.com>
2022-02-21 10:44:49 +0300

    Добавьте файлы проекта.

---
+++ SHA256Hash/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SHA256Hash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
